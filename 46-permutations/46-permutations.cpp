c219c8529a69dace317bc97d977ff515c6d54444
spartty <65155903+leehyunk6310@users.noreply.github.com>
2022-08-17 17:13:11 +0900

    Time: 14 ms (8.28%), Space: 9 MB (6.43%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> ans;    
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> path;
+        vector<bool> visited(nums.size(), false);
+        dfs(nums, path, visited);
+        
+        return ans;
+        
+    }
+    
+    void dfs(vector<int>& nums, vector<int> path, vector<bool> visited)
+    {
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (path.size() == nums.size())
+            {
+                ans.push_back(path);
+                return;
+            }
+            
+            if (!visited[i])
+            {
+                visited[i] = true;
+                path.push_back(nums[i]);
+                dfs(nums, path, visited);
+                path.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+    
+    
+    
+};
